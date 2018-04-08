83d041483d07556d2375be580b2bb679eadcbcc6
mugala <mugalalevi21@gmail.com>
2018-04-08 22:52:22 +0300

    Getting the API call This commit will get the days news from our api

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from .request import get_news
 from app import app
 
 # Views
@@ -8,9 +9,12 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
+    
+     # Getting news
+    days_news = get_news('D_news')
+    print(days_news)
     title = 'Home - Get The News at your Convenience when they Break'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, D_news = days_news)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
@@ -20,3 +24,7 @@ def news(news_id):
     '''
 
     return render_template('news.html',id = news_id)
+
+
+from .request import get_movies
+
