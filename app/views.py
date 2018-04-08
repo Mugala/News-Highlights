e834ba1219668900a123f816dda24001ae10886b
mugala <mugalalevi21@gmail.com>
2018-04-08 20:39:07 +0300

    Dynamic Routes and configuration clases This commit will add the dynamic routes for the news pages as well as add the configuaration class for both production and development

--- app/views.py
+++ app/views.py
@@ -9,5 +9,13 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
+    message = 'Hello new world?'
     return render_template('index.html',message = message)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
