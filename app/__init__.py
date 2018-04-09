4f93b894d9b66f54f508948338e0b498f7eaad6a
mugala <mugalalevi21@gmail.com>
2018-04-10 00:23:10 +0300

    Restructuring This commit will move the config.py to the root folder import os create a dictionary config_option help in accessing different configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap ()
+
+def create_app(config_name):
 
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
+app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+# creating the app configuration
+app.config.from_object(config_options[config_name])
+)
 
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
 
-from app import views
+return app
--- config.py
+++ config.py
@@ -1,11 +1,14 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL ='https://newsapi.org/v2/{}?apiKey={}'
-
     ARTICLES_API_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
-    
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 
 
@@ -28,3 +31,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
