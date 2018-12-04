a67be0a8bec4f6568dd1623548128bec3d643483
mugala <mugalalevi21@gmail.com>
2018-12-04 16:16:11 +0300

    Virtual env and README THis commit will rectify some errors on the readme as well as change the venv to use python3

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/levi/Desktop/News-Highlight/virtual"
+set -gx VIRTUAL_ENV "/home/levi/Desktop/News-Highlights/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
