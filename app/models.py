74c52f2d95d35180ceb630e4b3f124b80a864df6
mugala <mugalalevi21@gmail.com>
2018-12-04 16:33:05 +0300

    models.py This commit will add two more models for topic and headlines

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 class News:
     '''
-    News class to define News Objects
+    News class to define News Object
     '''
 
     def __init__(self,id,name,description,url,category,language,country):
@@ -12,18 +12,45 @@ class News:
         self.category = category
         self.language = language
         self.country = country
-      
+
 class Article:
     '''
-    News class to define Articles Objects
+    Article class to define Articles objects
     '''
 
     def __init__(self,author,title,description,url,urlToImage,publishedAt):
-        
+
         self.author = author
         self.title = title
         self.description = description
         self.url = url
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
-      
+
+class Topic:
+    '''
+    Topic class to define Topic objects
+    '''
+
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+
+class Headline:
+    '''
+    Headline class to define Headline objects
+    '''
+
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
