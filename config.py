0512dc5a6aa82a272f8ba12ac01b9cae9012dd50
mugala <mugalalevi21@gmail.com>
2018-12-04 16:30:27 +0300

    manage.py config.py and test This commit will update the manage and config.py as well as create the folder for test

--- config.py
+++ config.py
@@ -1,38 +1,19 @@
 import os
 
+
 class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/{}?apiKey={}'
-    ARTICLES_API_BASE_URL='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
+class ProdConfig(Config):
+    pass
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
+config_options ={"production":ProdConfig,"default":DevConfig}
--- manage.py
+++ manage.py
@@ -1,19 +1,17 @@
-from app import create_app
 from flask_script import Manager,Server
+from app import create_app
 
-# Creating app instance
-app = create_app('development')
+app = create_app('default')
 
 manager = Manager(app)
 
-manager.add_command('server',Server)
-
+manager.add_command('server', Server)
 @manager.command
 def test():
-    """Run the unit tests."""
+    """Run the unit test."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity = 2).run(tests)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
