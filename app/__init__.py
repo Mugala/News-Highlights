d9e03784805fbb21ee8511ed2cfd04d2ba7d3bf6
mugala <mugalalevi21@gmail.com>
2018-12-04 16:34:29 +0300

    __init__.py This commit will update the __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,27 +2,21 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 
-bootstrap = Bootstrap ()
 
+bootstrap = Bootstrap()
 
-
-# Initializing application
 def create_app(config_name):
     app = Flask(__name__)
 
-    # creating the app configuration
+    #Creating the app configurations
     app.config.from_object(config_options[config_name])
-    
 
-    # Initializing Flask Extensions
+    #Initializing flask extensions
     bootstrap.init_app(app)
 
-
-    #Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting config
     from .request import configure_request
     configure_request(app)
 
